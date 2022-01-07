3937548ab1929f441bee74c13796591fc0feefca
namkornfour <namkornfour@gmail.com>
2022-01-07 16:47:04 +0700

    edit index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var ncp = require('ncp').ncp;
 
 var src = './web-backend';
 var des = './published';
+var message = "Good Lift";
 
 ncp(src, des, (err) => {
     if(err) return console.log(err);    
