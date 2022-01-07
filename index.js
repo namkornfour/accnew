1fd8b2ecfa54666854e7679a6198ce5ddb1f5846
namkornfour <namkornfour@gmail.com>
2022-01-07 16:57:32 +0700

    edit data in message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var ncp = require('ncp').ncp;
 
 var src = './web-backend';
 var des = './published';
-var message = "Good Lift";
+var message = "Good Life";
 
 ncp(src, des, (err) => {
     if(err) return console.log(err);    
